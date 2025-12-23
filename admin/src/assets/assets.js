c105ec866e53841e6f1cab10f1d5604ab8e1d64a
Rakhal Chandra Mondal <Rakhalchandro@gmail.com>
2025-12-23 12:36:21 +0600

    Update API URL to production server

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,5 +14,5 @@ export const assets ={
     parcel_icon
 }
 
-// export const url = 'https://joykoly-server.onrender.com'
-export const url = 'http://localhost:4000'
+ export const url = 'https://joykoly-server.onrender.com'
+//export const url = 'http://localhost:4000'
